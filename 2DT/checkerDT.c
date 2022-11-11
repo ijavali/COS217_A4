bd776214b5837464d2fc3d4d6210bd3321c47a28
JackCZhang <jz4267@princeton.edu>
2022-11-11 12:31:08 -0500

    m

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -124,6 +124,15 @@ boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
         }
     }
         
+    
+   if(oNRoot)
+   {
+      if(ulCount != Node_getNumChildren(oNRoot))
+      {
+         fprintf(stderr, "Number of children does not match the size of the tree\n");
+         return FALSE;
+      }
+   }
   
 
    /* Now checks invariants recursively at each node from the root. */
