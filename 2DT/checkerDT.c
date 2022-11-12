9e365824b7c39d767ae2b84f36170e3941eac780
JackCZhang <jz4267@princeton.edu>
2022-11-12 15:40:00 -0500

    m

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -54,19 +54,19 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
          }
       
       /* parent must not already have child with this path */
-       /* if(Node_hasChild(oNParent, oPNPath, &ulIndex)) {
+       /*if(Node_hasChild(oNParent, oPNPath, &ulIndex)) {
             fprintf(stderr, "A parent already has a child with this path\n");
             return FALSE;
          }*/
 
       
       /* all children under a parent must be in lexigraphical order*/
-        /* for(ulIndex = 0; ulIndex < Node_getNumChildren(oNParent) - 1; ulIndex++)
+        for(ulIndex = 0; ulIndex < Node_getNumChildren(oNParent) - 1; ulIndex++)
          {
             Node_T prev = NULL;
             Node_T next = NULL;
-            int iStatusPrev = Node_getChild(oNNode, ulIndex, &prev);
-            int iStatusNext = Node_getChild(oNNode, ulIndex + 1, &next);
+            int iStatusPrev = Node_getChild(oNParent, ulIndex, &prev);
+            int iStatusNext = Node_getChild(oNParent, ulIndex + 1, &next);
             if(iStatusPrev != SUCCESS) {
                fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
                return FALSE;
@@ -80,7 +80,7 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
                fprintf(stderr, "The children of a parent are not in lexigraphical order\n");
                return FALSE;
             }
-         }*/
+         }
             
    }
    else
